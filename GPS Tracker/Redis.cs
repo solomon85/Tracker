5abe6717689c1615e3b808bbdcf54c37915daa8b
solomon85 <reza.yy@gmail.com>
2024-01-19 21:56:59 +0330

    Initial commit

---
+++ GPS Tracker/Redis.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPS_Tracker
+{
+    public class Redis
+    {
+        public static IDatabase redis;
+        public static void Configure()
+        {
+            var configuration = ConfigurationOptions.Parse("localhost:6379");
+            var redisConnection = ConnectionMultiplexer.Connect(configuration);
+            redis = redisConnection.GetDatabase();
+        }
+
+        public static void SetCacheData(string key, string value)
+        {
+            redis.StringSet(key, value);
+        }
+
+        public static bool KeyDelete(string key) => redis.KeyDelete(key);
+        public static String GetCacheData(string key) => redis.StringGet(key);
+        public static T GetCacheData<T>(string key) => (T)Convert.ChangeType(redis.StringGet(key), typeof(T));
+        
+    }
+}
